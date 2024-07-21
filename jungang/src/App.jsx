5303aa57fe3805040eef332ef07273e3d1559774
ttatjwi <su_zero_@naver.com>
2024-07-21 15:13:18 +0900

    [enh] 로그인 페이지 구현

---
+++ jungang/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import LoginPage from './components/LoginPage.jsx'
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default App
