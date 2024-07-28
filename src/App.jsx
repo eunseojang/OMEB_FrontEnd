d908b984d36a9ebde295972d0be143accae48b5b
ttatjwi <su_zero_@naver.com>
2024-07-28 20:37:56 +0900

    [chore] 로그인 페이지 돔 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
 import LoginPage from './components/LoginPage.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </Router>
   )
 }
 
