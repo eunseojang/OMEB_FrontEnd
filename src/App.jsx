177789a4213ab8c3344da8c1980a9b698bf86dad
eunseo <jangeun4097@naver.com>
2024-07-29 13:34:32 +0900

    detailpage 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Main from "./components/Main";
 import Mainpage from "./pages/Mainpage";
 import Mypages from "./pages/Mypage";
 import "./App.css";
-import LankingPage from "./Rank/RankingPage";
+import RankingPage from "./Rank/RankingPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailPage from "./Detail/DetailPage";
 
@@ -13,8 +13,8 @@ function App() {
         <Route Path="/" element={<Main />}>
           <Route index element={<Mainpage />} />
           <Route path="Mypages" element={<Mypages />} />
-          <Route path="LankingPage" element={<LankingPage />} />
-          <Route path="Detail" element={<DetailPage />} />
+          <Route path="Ranking" element={<RankingPage />} />
+          <Route path="Detail/:bookId" element={<DetailPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
