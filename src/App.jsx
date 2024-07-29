e8c76f9d654abed153e095cccaa4605bd59f57fe
ttatjwi <su_zero_@naver.com>
2024-07-29 14:11:31 +0900

    [enh] 로그인 페이지 구현 및 돔 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import './App.css';
 import Header from './components/Header';
 import Mainpage from './pages/Mainpage';
 import Mypages from './pages/Mypage';
-import './App.css';
 import LankingPage from './Rank/RankingPage';
 import LoginPage from './components/LoginPage.jsx'
+import RecommendPage from './components/RecommendPage.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Mainpage />
-      {/* <Mypages /> */}
-      {/* <LankingPage /> */}
-    </>
+    // <>
+    //   <Header />
+    //   <Mainpage />
+    //   {/* <Mypages /> */}
+    //   {/* <LankingPage /> */}
+    // </>
 
     <Router>
       <Routes>
+        <Route path='/' element={<Mainpage />} />
         <Route path='/login' element={<LoginPage />} />
       </Routes>
     </Router>
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
 import './LoginPage.css'
-import google_logo from '../assets/google_logo.png'
-import naver_logo from '../assets/naver_logo.png'
-import kakao_logo from '../assets/kakao_logo.png'
+import google_logo from '../assets/logo/google_logo.png'
+import naver_logo from '../assets/logo/naver_logo.png'
+import kakao_logo from '../assets/logo/kakao_logo.png'
 
 
 const LoginPage = () => {
@@ -11,55 +11,58 @@ const LoginPage = () => {
   const [password, setPassword] = useState("")
 
   return (
-    <div className="login">
-      <h4>일심동책</h4>
-      <form>
-        <div className="text_area">
+    <div className="back">
+      <div className="login">
+        <h4>일심동책</h4>
+        <form>
+          <div className="text_area">
+            <input
+              type='text'
+              id='email'
+              name='email'
+              defaultValue={email} 
+              className='text_input'
+            />
+          </div>
+          <div className="text_area">
+            <input
+              type='password'
+              id='password'
+              name='password'
+              defaultValue={password}
+              className='text_input'
+            />
+          </div>
           <input
-            type='text'
-            id='email'
-            name='email'
-            defaultValue={email} 
-            className='text_input'
+            type='submit'
+            value= "로그인"
+            className='btn'
           />
-        </div>
-        <div className="text_area">
-          <input
-            type='password'
-            id='password'
-            name='password'
-            defaultValue={password}
-            className='text_input'
-          />
-        </div>
-        <input
-          type='submit'
-          value= "로그인"
-          className='btn'
-        />
-      </form>
+        </form>
 
-      <div className="social_login">
-        <p>SNS 간편 로그인</p>
-        <div className="social">
-          <img src={google_logo} alt="google_logo" />
-          <div className="circle"></div>
+        <div className="social_login">
+          <p>SNS 간편 로그인</p>
+          <div className="social">
+            <img src={kakao_logo} alt="kakao_logo" />
+          </div>
+          <div className="social">
+            <img src={naver_logo} alt="naver_logo" />
+          </div>
+          <div className="social">
+            <img src={google_logo} alt="google_logo" />
+            <div className="circle"></div>
+          </div>
         </div>
-        <div className="social">
-          <img src={naver_logo} alt="naver_logo" />
+        
+        <div className="sub">
+          <a className='link' href='/'>회원가입</a>
+          <a className='link' href='/'>비밀번호 찾기</a>
+          <a className='link' href='/'>아이디 찾기</a>
         </div>
-        <div className="social">
-          <img src={kakao_logo} alt="kakao_logo" />
-        </div>
+        
       </div>
-      
-      <div className="sub">
-        <a className='link' href='/'>회원가입</a>
-        <a className='link' href='/'>아이디 찾기</a>
-        <a className='link' href='/'>비밀번호 찾기</a>
-      </div>
-      
     </div>
+
   );
 };
 
